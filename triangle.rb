ece02121f814bdc80db3760a778a26d571d3a659
Grant George <grantelliottgeorge@gmail.com>
2014-12-15 23:30:36 -0500

    166

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,19 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == 0 || b == 0 || c == 0
+  raise TriangleError, "Any side length cannot equal 0"
+  elsif a < 0 || b < 0 || c < 0
+  raise TriangleError, "Side length cannot be less than 0"
+  elsif a + b <= c || a + c <= b || b + c < a
+  raise TriangleError, "Side error. 2 sides must add up to > other side."
+  end
+  if [a,b,c].uniq.size == 1
+  return :equilateral
+  elsif [a,b,c].uniq.size == 2
+  return :isosceles
+  else return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
